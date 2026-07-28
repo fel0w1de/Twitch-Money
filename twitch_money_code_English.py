499e9899a4879fc0b55383287221fc8fca0728e8
fel0w1de <iv.pivka@gmail.com>
2026-07-29 02:22:42 +0300

    Added English code

---
+++ twitch_money_code_English.py
@@ -0,0 +1,16 @@
+
+points = int(input("Enter the number of points awarded: "))
+multiplier = float(input("Enter the betting odds: "))
+
+
+total_payout = int(points * multiplier)
+net_profit = total_payout - points
+
+
+print(f"\n--- Betting results ---")
+print(f"You will receive in total: {total_payout} points")
+print(f"The net gain will be: {net_profit} points")
+
+
+
+input("\nPress Enter to close the program...")
